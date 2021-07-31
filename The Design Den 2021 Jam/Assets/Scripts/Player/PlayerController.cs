fb5ef65b7dcd1e63e356f1ff3dd4de1301bd1a2b
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2021-07-31 20:44:35 +0200

    3rd Movement Iteration (only movement left is rpm bonus)

--- The Design Den 2021 Jam/Assets/Scripts/Player/PlayerController.cs
+++ The Design Den 2021 Jam/Assets/Scripts/Player/PlayerController.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    Vector2 currentDir;
 
     [Range(0.01f, 50.0f)]
     public float speed = 10.0f;
 
     [Range(0.0f, 1.0f)] //1 drift, cannot change direction - 0 drift changes direction right away
     public float drift = 0.5f; //percentatge of slerp between current dir and new input dir
+    [Range(0.0f, 10.0f)]
+    public float drag = 1.0f;
+    Vector2 vel = Vector2.zero;
+    Vector2 pos = Vector2.zero;
 
-
-    Vector2 vel;
-    Vector2 pos;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        currentDir = Vector2.zero;
+        pos = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
@@ -40,17 +40,12 @@ public class PlayerController : MonoBehaviour
 
         if (newDir != Vector2.zero)
         {
-            currentDir = Vector2.Lerp(currentDir, newDir, (1.0f - drift) * Time.deltaTime);
+            vel += newDir * speed * Time.deltaTime*(1.0f-drift);
         }
 
-
-        if (currentDir.magnitude > 1.0f)
-        {
-            currentDir.Normalize();
-        }
-
-        //currentDir -= currentDir * drag*Time.deltaTime;
-        gameObject.transform.position = gameObject.transform.position + new Vector3(currentDir.x, currentDir.y, 0) * speed * Time.deltaTime;
+        vel -= vel * drag * Time.deltaTime;//Drag?
+        pos += vel * Time.deltaTime;
+        gameObject.transform.position = new Vector3(pos.x, pos.y, gameObject.transform.position.z);
 
     }
 
@@ -58,6 +53,6 @@ public class PlayerController : MonoBehaviour
 
     public void AddPush(float pushForce, Vector2 origin)
     {
-        vel += new Vector2(transform.position.x-origin.x,transform.position.y-origin.y).normalized * pushForce * Time.deltaTime;
+        vel += new Vector2(transform.position.x - origin.x, transform.position.y - origin.y).normalized * pushForce * Time.deltaTime;
     }
 }
