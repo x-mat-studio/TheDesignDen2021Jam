be722100f2f512c30aa64d60b700e7fbba549806
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-07-31 23:12:23 +0200

    grid test

---
+++ The Design Den 2021 Jam/Assets/Scripts/GridCreation.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreation : MonoBehaviour
+{
+    public GameObject gridPrefab = null;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag != "Grid")
+            GenerateGrids();
+    }
+
+
+    private void GenerateGrids()
+    {
+        //UP
+        Vector2 nord = new Vector2(0.0f, 1.0f);
+
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 10), nord);
+        
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, 0.0f);
+        }
+
+        Vector2 nordEast = new Vector2(1.0f, 1.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x + 10, gameObject.transform.position.y + 10), nordEast);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x + 10, gameObject.transform.position.y + 10, 0.0f);
+        }
+
+        Vector2 nordWest = new Vector2(-1.0f, 1.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x - 10, gameObject.transform.position.y + 10), nordWest);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x - 10, gameObject.transform.position.y + 10, 0.0f);
+        }
+
+        //LATERALS
+        Vector2 east = new Vector2(1.0f, 0.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x + 10, gameObject.transform.position.y), east);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x + 10, gameObject.transform.position.y, 0.0f);
+        }
+
+        Vector2 west = new Vector2(-1.0f, 0.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x - 10, gameObject.transform.position.y), nordWest);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x - 10, gameObject.transform.position.y, 0.0f);
+        }
+
+        //DOWN
+
+        Vector2 sud = new Vector2(0.0f, -1.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 10), sud);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 10, 0.0f);
+        }
+
+        Vector2 sudEast = new Vector2(1.0f, -1.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x + 10, gameObject.transform.position.y - 10), sudEast);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x + 10, gameObject.transform.position.y - 10, 0.0f);
+        }
+
+        Vector2 sudWest = new Vector2(-1.0f, -1.0f);
+
+        hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x - 10, gameObject.transform.position.y - 10), sudWest);
+
+        if (hit.collider == null)
+        {
+            GameObject grid = Instantiate(gridPrefab);
+            grid.transform.position = new Vector3(gameObject.transform.position.x - 10, gameObject.transform.position.y - 10, 0.0f);
+        }
+    }
+}
